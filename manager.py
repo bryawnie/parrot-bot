99f7800a2a0a4a4f913c1255917d6d62d338a83f
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-07-21 23:11:52 -0400

    Bot initialization creates database once

--- manager.py
+++ manager.py
@@ -1,23 +1,30 @@
 from sqlite3 import connect
 from bot import runner
 from dotenv import load_dotenv
-from os import environ
+from os import environ, path
 
 load_dotenv()
+DB_PATH = "db.sqlite"
 
-def init(db_name='db.sqlite'):
-    with open(db_name, 'w') as db:
-        db.close()
+def create_file(filename):
+    with open(filename, 'w') as f:
+        f.close()
 
-    con = connect("db.sqlite")
+def init_database(db_path: str):
+    create_file(db_path)
+
+    con = connect(db_path)
     cur = con.cursor()
 
-    cur.execute("CREATE TABLE phrases (hash, author, phrase, added_by, added_at)")
+    # We will restrict the shared content to only conversations
+    # with same chat id (or group id)
+    cur.execute("CREATE TABLE phrases (chat_id, hash, author, phrase, added_at)")
     con.commit()
 
 def run_bot():
     runner(token=environ.get('BOT_TOKEN'))
 
 if __name__ == '__main__':
-    # init()
+    if not path.exists(DB_PATH):
+        init_database(DB_PATH)
     run_bot()
